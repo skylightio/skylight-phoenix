857e87a884e9dc46224afa28cdeabcf3e71e3b3e
Andrea Leopardi <an.leopardi@gmail.com>
2015-11-17 16:39:54 +0100

    Implement a defnif macro for easier NIF definition

--- lib/skylight/nif.ex
+++ lib/skylight/nif.ex
@@ -1,42 +1,67 @@
+defmodule Skylight.NIF.Macros do
+  @moduledoc false
+
+  @doc """
+  Defines a failing clause for the given function.
+
+  The utility of this macro (instead of just using `{:my_fun, arity}`) is that
+  it allows for much better documentation and specs because every `defnif` can
+  be preceded by @docs and @specs.
+
+  ## Examples
+
+      @doc "Does stuff"
+      @spec my_nif(term, term) :: term
+      defnif my_nif(arg1, arg2)
+
+  """
+  defmacro defnif(fun_signature) do
+    {fun, args} = Macro.decompose_call(fun_signature)
+    args = underscorize_args(args)
+
+    quote do
+      def unquote(fun)(unquote_splicing(args)) do
+        raise "NIF #{unquote(fun)}/#{unquote(length(args))} not implemented"
+      end
+    end
+  end
+
+  defp underscorize_args(args) do
+    for {name, meta, nil} <- args do
+      {String.to_atom("_" <> Atom.to_string(name)), meta, nil}
+    end
+  end
+end
+
 defmodule Skylight.NIF do
   @moduledoc false
 
+  import Skylight.NIF.Macros
+
   @on_load :load_nifs
+
+  defnif load_libskylight(path)
+  defnif hrtime()
+  defnif instrumenter_new(env)
+  defnif instrumenter_start(inst)
+  defnif instrumenter_stop(inst)
+  defnif instrumenter_submit_trace(inst, trace)
+  defnif instrumenter_track_desc(inst, endpoint, desc)
+  defnif trace_new(start, uuid, endpoint)
+  defnif trace_start(trace)
+  defnif trace_endpoint(trace)
+  defnif trace_set_endpoint(trace, endpoint)
+  defnif trace_uuid(trace)
+  defnif trace_set_uuid(trace, uuid)
+  defnif trace_instrument(trace, time, category)
+  defnif trace_span_set_title(trace, handle, title)
+  defnif trace_span_set_desc(trace, handle, desc)
+  defnif trace_span_done(trace, handle, time)
+  defnif lex_sql(sql)
 
   # Loads the .so file that contains the NIFs.
   def load_nifs() do
     :erlang.load_nif(nif_path(), 0)
-  end
-
-  # Add default clauses for all NIFs. The default clauses just raise an
-  # exception.
-  nifs = [
-    {:load_libskylight, 1},
-    {:hrtime, 0},
-    {:instrumenter_new, 1},
-    {:instrumenter_start, 1},
-    {:instrumenter_stop, 1},
-    {:instrumenter_submit_trace, 2},
-    {:instrumenter_track_desc, 3},
-    {:trace_new, 3},
-    {:trace_start, 1},
-    {:trace_endpoint, 1},
-    {:trace_set_endpoint, 2},
-    {:trace_uuid, 1},
-    {:trace_set_uuid, 2},
-    {:trace_instrument, 3},
-    {:trace_span_set_title, 3},
-    {:trace_span_set_desc, 3},
-    {:trace_span_done, 3},
-    {:lex_sql, 1},
-  ]
-
-  for {name, arity} <- nifs do
-    args = List.duplicate(quote(do: _), arity)
-
-    def unquote(name)(unquote_splicing(args)) do
-      raise "NIF #{unquote(name)}/#{unquote(arity)} not implemented"
-    end
   end
 
   defp nif_path() do
