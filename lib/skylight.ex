6e0754b59930d4c842cbd22deb708ef809424c92
Andrea Leopardi <an.leopardi@gmail.com>
2015-11-17 16:48:04 +0100

    Choose the extension of SO files based on the os
    We're now checking for the current OS (with `:os.type/1`) in order to
    choose the extension for SO files (e.g., "dylib" for SO files on OSX).

--- lib/skylight.ex
+++ lib/skylight.ex
@@ -11,11 +11,19 @@ defmodule Skylight do
   end
 
   defp load_libskylight!() do
-    path = Application.app_dir(:skylight, "priv") |> Path.join("libskylight.dylib")
+    path = Application.app_dir(:skylight, "priv") |> Path.join("libskylight.#{so_ext()}")
 
     case Skylight.NIF.load_libskylight(path) do
       {:ok, _} -> :ok
       :error   -> raise "couldn't load libskylight from #{path}"
+    end
+  end
+
+  defp so_ext() do
+    # TODO include Windows in this (with a .dll extension).
+    case :os.type() do
+      {:unix, :darwin} -> "dylib"
+      {:unix, _}       -> "so"
     end
   end
 end
