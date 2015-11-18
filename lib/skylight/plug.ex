102ad166eff4eb47451bcb1b165e4fff930418c2
Andrea Leopardi <an.leopardi@gmail.com>
2015-11-18 17:33:19 +0100

    Create, instrument and submit traces in the plug

--- lib/skylight/plug.ex
+++ lib/skylight/plug.ex
@@ -3,19 +3,76 @@ defmodule Skylight.Plug do
 
   require Logger
 
+  alias Skylight.Trace
+  alias Skylight.Instrumenter
+  alias Skylight.InstrumenterAgent
+
   import Plug.Conn
+
+  defp debug(conn, str) do
+    Logger.debug "(#{conn.request_path}) #{str}"
+  end
 
   def init(opts) do
     opts
   end
 
   def call(conn, _opts) do
-    req_start_timestamp = :os.timestamp()
-    register_before_send(conn, &before_send(&1, req_start_timestamp))
+    trace = Trace.new("fake#endpoint")
+    whole_req_handle = Trace.instrument(trace, "whole_req")
+
+    Logger.debug "Created a new trace for request at \"#{conn.request_path}\": #{inspect trace}"
+
+    conn
+    |> put_private(:skylight_trace, trace)
+    |> put_new_trace_handle(:whole_req, whole_req_handle)
+    |> register_before_send(&before_send/1)
   end
 
-  defp before_send(conn, req_start_timestamp) do
-    diff = :timer.now_diff(:os.timestamp(), req_start_timestamp)
-    put_private(conn, :skylight_resp_time, diff)
+  def controller_hook(conn, _opts) do
+    {trace, _handle} = get_trace_and_handle(conn, :whole_req)
+    :ok = Trace.put_endpoint(trace, get_route(conn))
+
+    Logger.debug "Changed the endpoint of the current trace in the controller: #{inspect trace}"
+
+    conn
+  end
+
+  defp before_send(conn) do
+    {trace, handle} = get_trace_and_handle(conn, :whole_req)
+
+    # Clean up the connection.
+    conn =
+      conn
+      |> put_private(:skylight_trace, nil)
+      |> put_private(:skylight_trace_handles, %{})
+
+    :ok = Trace.mark_span_as_done(trace, handle)
+    :ok = Instrumenter.submit_trace(inst(), trace)
+
+    # Oh god, state is hard. Here, we can't inspect the trace (or really, use it
+    # in any way) because submitting it made Rust free it. :(
+
+    conn
+  end
+
+  defp inst() do
+    InstrumenterAgent.get()
+  end
+
+  defp put_new_trace_handle(conn, name, handle) do
+    handles = conn.private[:skylight_trace_handles] || %{}
+    handles = Map.put(handles, name, handle)
+    put_private(conn, :skylight_trace_handles, handles)
+  end
+
+  defp get_trace_and_handle(conn, name) do
+    {conn.private[:skylight_trace], conn.private[:skylight_trace_handles][name]}
+  end
+
+  defp get_route(conn) do
+    controller = conn.private[:phoenix_controller]
+    action = conn.private[:phoenix_action]
+    inspect(controller) <> "#" <> to_string(action)
   end
 end
