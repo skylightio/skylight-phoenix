fb4b46fb96ada765609dfaa0f84efca35101521f
Andrea Leopardi <an.leopardi@gmail.com>
2015-11-24 18:36:15 +0100

    Proxy Skylight.Ecto.Repo to the actual repo
    Instrumentation is done in Skylight.Ecto.Repo but everything else is
    proxied to the original repo (specified in the options to `use
    Skylight.Ecto.Repo`).

--- lib/skylight/ecto/repo.ex
+++ lib/skylight/ecto/repo.ex
@@ -1,9 +1,93 @@
 defmodule Skylight.Ecto.Repo do
-  defmacro __using__(_opts) do
-    quote do
-      def log(log_entry) do
-        :ok = Skylight.Ecto.register_log_entry(log_entry)
-        super(log_entry)
+  defmacro __using__(opts) do
+    quote bind_quoted: [opts: opts] do
+      @proxy_repo Keyword.fetch!(opts, :proxy_to)
+
+      def config do
+        @proxy_repo.config
+      end
+
+      def start_link(opts \\ []) do
+        @proxy_repo.start_link(opts)
+      end
+
+      def stop(pid, timeout \\ 5000) do
+        @proxy_repo.stop(pid, timeout)
+      end
+
+      def all(queryable, opts \\ []) do
+        instrument queryable, fn -> @proxy_repo.all(queryable, opts) end
+      end
+
+      def get(queryable, id, opts \\ []) do
+        instrument queryable, fn -> @proxy_repo.get(queryable, id, opts) end
+      end
+
+      def get!(queryable, id, opts \\ []) do
+        instrument queryable, fn -> @proxy_repo.get!(queryable, id, opts) end
+      end
+
+      def get_by(queryable, clauses, opts \\ []) do
+        instrument queryable, fn -> @proxy_repo.get_by(queryable, clauses, opts) end
+      end
+
+      def get_by!(queryable, clauses, opts \\ []) do
+        instrument queryable, fn -> @proxy_repo.get!(queryable, clauses, opts) end
+      end
+
+      def one(queryable, opts \\ []) do
+        instrument queryable, fn -> @proxy_repo.one(queryable, opts) end
+      end
+
+      def one!(queryable, opts \\ []) do
+        instrument queryable, fn -> @proxy_repo.one!(queryable, opts) end
+      end
+
+      def update_all(queryable, updates, opts \\ []) do
+        instrument queryable, :update_all, fn -> @proxy_repo.update_all(queryable, updates, opts) end
+      end
+
+      def delete_all(queryable, opts \\ []) do
+        instrument queryable, :delete_all, fn -> @proxy_repo.delete_all(queryable, opts) end
+      end
+
+      def insert(model, opts \\ []) do
+        instrument model, :insert, fn -> @proxy_repo.insert(model, opts) end
+      end
+
+      def update(model, opts \\ []) do
+        instrument model, :update, fn -> @proxy_repo.update(model, opts) end
+      end
+
+      def delete(model, opts \\ []) do
+        instrument model, :delete, fn -> @proxy_repo.delete(model, opts) end
+      end
+
+      def insert!(model, opts \\ []) do
+        instrument model, :insert, fn -> @proxy_repo.insert!(model, opts) end
+      end
+
+      def update!(model, opts \\ []) do
+        instrument model, :update, fn -> @proxy_repo.update!(model, opts) end
+      end
+
+      def delete!(model, opts \\ []) do
+        instrument model, :delete, fn -> @proxy_repo.delete!(model, opts) end
+      end
+
+      def preload(model_or_models, preloads) do
+        @proxy_repo.preload(model_or_models, preloads)
+      end
+
+      # Functions with no default values, where we can use defdelegate.
+      defdelegate [__adapter__,
+                   __query_cache__,
+                   __repo__,
+                   __pool__,
+                   log(entry)], to: @proxy_repo
+
+      defp instrument(queryable, kind \\ :all, fun) do
+        Skylight.Ecto.instrument(@proxy_repo, kind, queryable, fun)
       end
     end
   end
