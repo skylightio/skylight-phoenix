fd516630142514a8783c1cd52a6edd5b2073e694
Andrea Leopardi <an.leopardi@gmail.com>
2015-11-24 18:35:46 +0100

    Add Skylight.Ecto.instrument/4 for queries

--- lib/skylight/ecto.ex
+++ lib/skylight/ecto.ex
@@ -1,8 +1,42 @@
-if :code.is_loaded(Ecto) do
+if Code.ensure_loaded?(Ecto) do
   defmodule Skylight.Ecto do
+    alias Skylight.Trace
 
-    def register_log_entry(%Ecto.LogEntry{} = _entry) do
-      :ok
+    @doc """
+    Instruments a function using the given `queryable` to find the title.
+
+    This function instruments the running of `fun`, using `repo`, `kind`, and
+    `queryable` to generate the SQL query (lexed) which is used as the title for
+    the new span.
+    """
+    @spec instrument(Ecto.Repo.t, atom, Ecto.Queryable.t, (() -> term)) :: :ok
+    def instrument(repo, kind, queryable, fun) do
+      trace = Trace.fetch()
+      handle = nil
+
+      if trace do
+        query = query_to_string(repo, kind, queryable)
+        handle = Trace.instrument(trace, "ecto.query")
+        :ok = Trace.set_span_title(trace, handle, query)
+        :ok = Trace.set_span_desc(trace, handle, query)
+      end
+
+      try do
+        fun.()
+      after
+        if trace do
+          :ok = Trace.mark_span_as_done(trace, handle)
+        end
+      end
+    end
+
+    defp query_to_string(repo, kind, queryable) when kind in ~w(all update_all delete_all)a do
+      {query, _} = Ecto.Adapters.SQL.to_sql(kind, repo, queryable)
+      query
+    end
+
+    defp query_to_string(_repo, _kind, queryable) do
+      inspect(queryable)
     end
   end
 end
