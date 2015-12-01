0445737d1bfd541eab6f7985e19dcca0930296d2
Andrea Leopardi <an.leopardi@gmail.com>
2015-12-01 20:12:24 +0100

    Make the example Phoenix app use Phoenix master

--- examples/my_app/mix.exs
+++ examples/my_app/mix.exs
@@ -30,7 +30,7 @@ defmodule MyApp.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.0.3"},
+    [{:phoenix, github: "phoenixframework/phoenix", override: true},
      {:phoenix_ecto, "~> 1.1"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
