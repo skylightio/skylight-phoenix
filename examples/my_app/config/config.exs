9c5e0a1e6e400a20db13ae4031eb1e14b1b7bf03
Andrea Leopardi <an.leopardi@gmail.com>
2015-12-02 19:12:24 +0100

    Use the new config management in the example app

--- examples/my_app/config/config.exs
+++ examples/my_app/config/config.exs
@@ -20,6 +20,10 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :skylight,
+  authentication: {:system, "DIREWOLF_PHOENIX_TOKEN"},
+  sockdir_path: "/tmp"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
