9b401d31a0376c4133290343620dc5413306f5f5
Peter Wagenet <peter.wagenet@gmail.com>
2017-01-05 10:02:57 -0800

    Add more methods for Ecto 2

--- lib/skylight/ecto/repo.ex
+++ lib/skylight/ecto/repo.ex
@@ -23,8 +23,22 @@ defmodule Skylight.Ecto.Repo do
           @proxy_repo.stop(pid, timeout)
         end
 
+        if function_exported?(@proxy_repo, :transaction, 3) do
+          def transaction(fun_or_multi, opts \\ []) do
+            @proxy_repo.transaction(fun_or_multi, opts);
+          end
+
+          defdelegate in_transaction?,  to: @proxy_repo
+          defdelegate rollback,         to: @proxy_repo
+        end
+
         def all(queryable, opts \\ []) do
           instrument fn -> @proxy_repo.all(queryable, opts) end
+        end
+
+        # Not sure if instrumentation will work correctly for this since it's lazy
+        def stream(queryable, opts \\ []) do
+          instrument fn -> @proxy_repo.stream(queryable, opts) end
         end
 
         def get(queryable, id, opts \\ []) do
@@ -51,6 +65,15 @@ defmodule Skylight.Ecto.Repo do
           instrument fn -> @proxy_repo.one!(queryable, opts) end
         end
 
+        def aggregate(queryable, aggregate, field, opts \\ [])
+            when aggregate in [:count, :avg, :max, :min, :sum] and is_atom(field) do
+          instrument fn -> @proxy_repo.aggregate(queryable, aggregate, field, opts) end
+        end
+
+        def insert_all(schema_or_source, entries, opts \\ []) do
+          instrument fn -> @proxy_repo.insert_all(schema_or_source, entries, opts) end
+        end
+
         def update_all(queryable, updates, opts \\ []) do
           instrument fn -> @proxy_repo.update_all(queryable, updates, opts) end
         end
@@ -67,6 +90,10 @@ defmodule Skylight.Ecto.Repo do
           instrument fn -> @proxy_repo.update(model, opts) end
         end
 
+        def insert_or_update(changeset, opts \\ []) do
+          instrument fn -> @proxy_repo.insert_or_update(changeset, opts) end
+        end
+
         def delete(model, opts \\ []) do
           instrument fn -> @proxy_repo.delete(model, opts) end
         end
@@ -79,20 +106,29 @@ defmodule Skylight.Ecto.Repo do
           instrument fn -> @proxy_repo.update!(model, opts) end
         end
 
+        def insert_or_update!(changeset, opts \\ []) do
+          instrument fn -> @proxy_repo.insert_or_update!(changeset, opts) end
+        end
+
         def delete!(model, opts \\ []) do
           instrument fn -> @proxy_repo.delete!(model, opts) end
         end
 
-        def preload(model_or_models, preloads) do
-          @proxy_repo.preload(model_or_models, preloads)
+        def preload(struct_or_structs, preloads, opts \\ [])
+        def preload(nil, _, _), do: nil
+        def preload(struct_or_structs, preloads, opts) do
+          @proxy_repo.preload(struct_or_structs, preloads, opts)
         end
 
         # Functions with no default values, where we can use defdelegate.
+        # TODO: It may become possible to use `defdelegate` for more functions as of
+        #   elixir 1.3 since that appears to support default values
         defdelegate __adapter__,      to: @proxy_repo
         defdelegate __query_cache__,  to: @proxy_repo
         defdelegate __repo__,         to: @proxy_repo
         defdelegate __pool__,         to: @proxy_repo
         defdelegate log(entry),       to: @proxy_repo
+        defdelegate load(schema_or_types, data), to: @proxy_repo
 
         defp instrument(fun) do
           Skylight.Ecto.instrument(@proxy_repo, fun)
